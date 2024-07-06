79cbc866fd9b8f2daa36ebdf265ee6982d6ae4aa
Tamal Sur <tamalsur14@gmail.com>
2024-07-06 19:50:55 +0530

    completed

--- src/components/TimeDate.jsx
+++ src/components/TimeDate.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-let currentDate=new Date()
+
+
 
 const TimeDate = () => {
+ 
+ const [currentDate,setCurrentDate]=  useState(new Date());
+
+ useEffect(()=>{
+ 
+  let intervalId=setInterval(()=>{
+    setCurrentDate(new Date());
+  },1000);
+  
+  return () =>{
+    clearInterval(intervalId);
+  }
+ },[]);
+
   return (
     <h2 className='text-red-500 text-4xl mt-2'>This is Current Date and Time - {currentDate.toLocaleDateString()}
      - {currentDate.toLocaleTimeString()}</h2>
